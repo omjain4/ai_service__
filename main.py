fc2da90b1684034278462e6627678492aae5a3f3
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-08-01 15:27:38 +0530

    dsvf

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import uvicorn
 app = FastAPI()
 
 # CORS for integration with frontend/backend
-origins = ["http://localhost:3000", "http://localhost:5001"]  # Adjust to your ports
+origins = ["http://localhost:3000", "http://localhost:5001", "https://ai-wardrobe-backend-production.up.railway.app" , "https://stylezap-wardrobe-ai.vercel.app/"]  # Adjust to your ports
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.post("/predict-category")
